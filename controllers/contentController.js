8c1159a2e4a714cb8ee09e8b1f9544c9ee005e5d
lucariggi92 <riggiluca92@gmail.com>
2026-03-12 21:18:26 +0100

    fix: content Controller query index

--- controllers/contentController.js
+++ controllers/contentController.js
@@ -7,11 +7,18 @@ function indexContent(req, res, next) {
 
     //request category-->voglio mostare i contents in base alle category
  
-    let query = `SELECT contents.* , GROUP_CONCAT(img.url ORDER BY img.img_position ASC) AS tutte_le_foto 
-    FROM contents 
-    LEFT JOIN img 
-    ON contents.id = img.content_id 
-    WHERE contents.is_visible =1`
+    let query = `SELECT contents.*, img_sub.cover_image
+        FROM contents
+        LEFT JOIN (
+            SELECT content_id, url AS cover_image
+            FROM img
+            WHERE (content_id, img_position) IN (
+                SELECT content_id, MIN(img_position)
+                FROM img
+                GROUP BY content_id
+            )
+        ) AS img_sub ON contents.id = img_sub.content_id
+        WHERE contents.is_visible = 1`
     
     //CREO UN ARRAY PARAMS DOVE AGGIUNGERE le Category
     let params = []
@@ -23,7 +30,7 @@ function indexContent(req, res, next) {
         params.push(category);
     }
     
-    query += "  GROUP BY contents.id";
+
 
 
     //CONNECTION
